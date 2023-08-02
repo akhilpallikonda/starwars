35248175b84a20a8849abd15605f54f69f22b770
Akhil Pallikonda <akhilpallikonda@gmail.com>
2023-08-02 23:21:17 +0530

    starship implementation

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,74 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Home.module.scss';
+import { useState, useEffect } from 'react';
+
+function Spinner(){
+  return <div className={styles.loader}></div>
+}
+
+function TrophyIcon(){
+  return <img className={styles.icon} width="24" height="24" src="https://img.icons8.com/color/96/trophy.png" alt="trophy"/>
+}
+
+function Starship({index,starship, maxFilms}){
+  return <li key={"title-" + index} className={styles.card}>
+    <h3> {starship.name}  <span> {maxFilms==starship.films.length &&  <TrophyIcon/> } </span> </h3>
+    <div className={styles.rowgrid}>
+    <div className={styles.grid}> <div>Model </div> <div>{starship.model} </div></div>
+    <div className={styles.grid}> <div>Number of films </div> <div>{starship.films.length} </div></div>
+    </div>
+    </li>
+}
 
 export default function Home() {
+  const [starships, setStarships] = useState([]);
+  const [maxFilms,setMaxFilms] = useState(0);
+  const [loading,setLoading] = useState(false);
+
+  const url="https://swapi.dev/api/starships/";
+  
+  const getStarships = async () => {
+    let response = await fetch(url);
+    let data = await response.json();
+    const filteredStarships = data.results.filter((item) => parseInt(item.crew) != NaN && parseInt(item.crew) < 10).sort((a,d)=> d-a);
+    setStarships(filteredStarships);
+    const max = Math.max(...filteredStarships.map(o => o.films.length), 0);
+    setMaxFilms(max);
+    setLoading(false);
+  }
+  const handleClick = () =>{
+    setLoading(true);
+    getStarships();
+  }
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Starwars</title>
+        <link rel="icon" href="https://img.icons8.com/ios/50/star-wars.png" />
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <img src="/starwars.svg" alt="Starwars" className={styles.logo} />
+        <div className={styles.title}>
+          Sample React JS project using SWAPI API
         </div>
+        <h2>
+        Results are filtered to starships with a crew size lesser than 10 and sorted by crew size
+        </h2>
+        <p className={styles.description}>
+          
+        The star ship that has featured in the most films will show a <span> <img className={styles.icon} width="48" height="48" src="https://img.icons8.com/color/48/trophy.png" alt="trophy"/></span>
+        </p>
+        <button className={styles.fetchButton} onClick={handleClick}> Get Starships</button>
+        { loading ? <Spinner/> :        
+        <ul className={styles.grid}>
+          {starships.map((starship, index) => {
+            return (
+              <Starship index={index} starship={starship} maxFilms={maxFilms}/>
+            )
+          })}
+        </ul>}
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
